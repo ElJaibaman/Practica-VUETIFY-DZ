45242d4b066cae8fb5899a3495c510555d2992bd
ElJaibaman <denis.zuniga.j@gmail.com>
2026-03-10 21:00:05 -0600

    CARGA - Denis Zuniga

--- PRACTICA-VUETIFY/app/Http/Controllers/EnrollmentsController.php
+++ PRACTICA-VUETIFY/app/Http/Controllers/EnrollmentsController.php
@@ -9,23 +9,23 @@ use Inertia\Inertia;
 class EnrollmentsController extends Controller
 {
     //
-     public function index()
-    {
-        //
+        public function index()
+        {
+            //
+    
+            //echo "Hola desde el controlador de Enrollments";
+            //$enrollments = Enrollment::all();
+            //dd($enrollments);
+            //return inertia::render('Enrollments/Index', [
+            //    //'enrollments' => $enrollments
+            //]);
 
-       // echo "Hola desde el controlador de enrollments";
-       // $enrollments = Enrollment::all();
-       // dd($enrollments);
-       // return inertia::render('Enrollment/Index', [
-        //    'enrollments' => $enrollments
-        //]);
-
-         return inertia::render('Enrollment/Index',);
-    }
-
-    public function create()
-    {
-        //
-        return inertia::render('Enrollment/Create');
-    }
+                return inertia::render('Enrollment/Index');
+        }
+    
+        public function create()
+        {
+            //
+            return inertia::render('Enrollment/Create');
+        }
 }
